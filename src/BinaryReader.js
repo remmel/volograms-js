74878ba19b1f53802433e648acb0958693ed1870
Remy Mellet <remy.mellet@gmail.com>
2023-05-10 13:48:32 +0200

    Remove commented code

--- src/BinaryReader.js
+++ src/BinaryReader.js
@@ -16,7 +16,6 @@ export const TypedArrays = {
 };
 
 export class BinaryReader {
-    /** */
     constructor(arrayBuffer) {
         this.array = new Uint8Array(arrayBuffer)
         this.dataView = new DataView(arrayBuffer)
@@ -38,16 +37,9 @@ export class BinaryReader {
         return val
     }
 
-
-    //        // let blob = await(response.blob())
-    //         // let version = new Uint16Array(arrayBuffer, 4, 1)// let dataView = new DataView(arrayBuffer)
     readInt32() {
         let val =  this.dataView.getInt32(this.cur,true)
         this.cur += 4 //move pointer 4 bytes further
-        // let val = 0
-        // for(let i=0; i<4; i++, this.cur++) {
-        //     val +=this.array[this.cur]<<i // * Math.pow(2,i*8) //TODO check the pow or use << instead
-        // }
         return val
     }
 
@@ -60,25 +52,6 @@ export class BinaryReader {
     readByte() {
         return this.array[this.cur++]
     }
-
-    // // https://stackoverflow.com/questions/42699162/javascript-convert-array-of-4-bytes-into-a-float-value-from-modbustcp-read
-    // static readSingle(data) {
-    //
-    //     //(new DataView(arrayBuffer)).getFloat32(75, true)
-
-    //     var data4 =  [this.array[this.cur++], this.array[this.cur++], this.array[this.cur++], this.array[this.cur++]]
-    //
-    //     var buf = new ArrayBuffer(4);
-    //     var view = new DataView(buf);
-    //
-    //     data4.forEach((b, i) => {
-    //         view.setUint8(i, b)
-    //     })
-    //
-    //     // Read the bits as a float; note that by doing this, we're implicitly
-    //     // converting it from a 32-bit float into JavaScript's native 64-bit double
-    //     return view.getFloat32(0, true);
-    // }
 
     readSingle(d){
         let val =  this.dataView.getFloat32(this.cur, true) //see IOBuffer.ts readFloat32
